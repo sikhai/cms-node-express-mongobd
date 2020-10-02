35814cd1acb8ce45f49a4ea4c54dfe46f4122365
sikhai <sikhaivuvan@gmail.com>
2020-10-02 22:49:27 +0700

    update

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -7,4 +7,76 @@ router.get('/', (req, res) => {
     res.render('student/addOrEdit', {
         viewTitle: 'Insert Student'
     })
+});
+
+router.post("/", (req, res) => {
+    if(req.body._id == ""){
+        insertRecord(req, res);
+    }else{
+        updateRecord(req, res);
+    }
+});
+
+function insertRecord(req, res){
+    var student = new Student();
+    student.fullName = req.body.fullname;
+    student.email = req.body.email;
+    stundent.phonenumber = req.body.phonenumber;
+    student.address = req.body.address;
+    student.save((err, doc) => {
+        if(!err){
+            res.redirect('student/list')
+        }else{
+            console.log('Error during insert: ' + err)
+        }
+    })
+}
+
+function updateRecord(req, res){
+    Student.findOneAndUpdate(
+        { _id: req.body._id },
+        req.body,
+        { new:true },
+        (err, doc) => {
+            if(!err){
+                req.redirect('student/list')
+            }else{
+                console.log('Error during update: ' + err)
+            }
+        }
+    );
+}
+
+router.get('/list', (req, res) => {
+    Student.find((req, docs) => {
+        if(!err) {
+            res.render('student/list', {
+                list: docs
+            })
+        }else{
+            console.log('Error in retrieval')
+        }
+    })
 })
+
+router.get("/:id", (req, res) => {
+    Student.findById(req.params.id, (req, res) => {
+        if(!err){
+            res.render("student/addOrEdit", {
+                viewTitle: "Update Student",
+                student: doc,
+            });
+            console.log(doc);
+        }
+    });
+});
+
+router.get("delete/:id", (req, res) => {
+    Student.findByIdAndRemove(req.params.id, (err, doc) => {
+        if(!err){
+            res.redirect("student/list")
+        }else{
+            console.log("Error in deletion" + err)
+        }
+    });
+});
--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-const express = require('express')
-const path = require('path')
-const handlebars = require('handlebars')
-const exphds = require('express-handlebars')
-const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
-const bodyparser = require('body-parser')
+require('./models/db')
 
-var app = express()
+const express = require('express');
+const path = require('path');
+const handlebars = require('handlebars');
+const exphds = require('express-handlebars');
+const {
+    allowInsecurePrototypeAccess,
+} = require('@handlebars/allow-prototype-access');
+const bodyparser = require('body-parser');
 
-app.use(bodyparser.urlencoded({extended: false}))
-app.use(bodyparser.json())
+const studentController = require("./controllers/studentController");
+
+var app = express();
+
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
 
 app.get('/', (req, res) => {
     res.send(`
     <h2>Xin chao ban da den voi Students Database</h2>
     <h3>Nhan vao de xem chi tiet <b><a href="/student/list">Database</a></b></h3>
     `)
-})
+});
 
 app.set('views', path.join(__dirname, '/views/'));
 
@@ -31,4 +37,5 @@ app.engine(
 
 app.listen(3000, () => {
     console.log("server stared at port 3000");
-})
+});
+
